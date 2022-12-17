5254add6d223d9103730ad7b7a64fd50fe42e11c
Matt Powell <96788296+mpowell773@users.noreply.github.com>
2022-12-17 15:56:28 -0500

    Exit Button Functionality
    Exit Button exits application and unity play mode

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// # symbol stands for compiler commands
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class UIManager : MonoBehaviour
 {
     public void StartNewGame()
     {
         SceneManager.LoadScene("Main");
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 }
